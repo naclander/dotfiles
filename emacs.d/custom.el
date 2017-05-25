545ce92682d302bdbabb5b9e4ca7562867e31275
Nathan Aclander <naclander@linux.ucla.edu>
2017-05-24 22:36:50 -0700

    More emacs changes

--- emacs.d/custom.el
+++ emacs.d/custom.el
@@ -23,11 +23,14 @@
    (vector "#c5c8c6" "#cc6666" "#b5bd68" "#f0c674" "#81a2be" "#b294bb" "#8abeb7" "#1d1f21"))
  '(custom-safe-themes
    (quote
-    ("3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "b9e9ba5aeedcc5ba8be99f1cc9301f6679912910ff92fdf7980929c2fc83ab4d" "84d2f9eeb3f82d619ca4bfffe5f157282f4779732f48a5ac1484d94d5ff5b279" "47aa6e82734866b2915781c6e1d9517bd897d45fe8aec360dd4b6294fec73068" "9d91458c4ad7c74cf946bd97ad085c0f6a40c370ac0a1cbeb2e3879f15b40553" "82d2cac368ccdec2fcc7573f24c3f79654b78bf133096f9b40c20d97ec1d8016" "06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" default)))
+    ("fa2b58bb98b62c3b8cf3b6f02f058ef7827a8e497125de0254f56e373abee088" "bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" "b747fb36e99bc7f497248eafd6e32b45613ee086da74d1d92a8da59d37b9a829" "1b8d67b43ff1723960eb5e0cba512a2c7a2ad544ddb2533a90101fd1852b426e" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "b9e9ba5aeedcc5ba8be99f1cc9301f6679912910ff92fdf7980929c2fc83ab4d" "84d2f9eeb3f82d619ca4bfffe5f157282f4779732f48a5ac1484d94d5ff5b279" "47aa6e82734866b2915781c6e1d9517bd897d45fe8aec360dd4b6294fec73068" "9d91458c4ad7c74cf946bd97ad085c0f6a40c370ac0a1cbeb2e3879f15b40553" "82d2cac368ccdec2fcc7573f24c3f79654b78bf133096f9b40c20d97ec1d8016" "06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" default)))
  '(fci-rule-color "#282a2e")
+ '(helm-ag-base-command "rg --no-heading")
+ '(helm-follow-mode-persistent t)
  '(package-selected-packages
    (quote
-    (auto-highlight-symbol hy-mode company-quickhelp nlinum rainbow-mode eyebrowse shackle zone-matrix zone-rainbow zone-nyan company-irony helm-cscope auto-complete-etags ag ggtags circe cider highlight-sexp highlight-parentheses zoom-frm fill-column-indicator powerline-evil ace-window evil-magit hydra helm-anything helm-projectile magit use-package projectile helm evil elpy color-theme-sanityinc-tomorrow)))
+    (evil-multiedit language-detection w3m info+ helm-flyspell helm-google helm-pydoc jedi ecukes undercover el-mock ert-runner helm-projectile helm-ag helm-cider helm info-buffer pdf-tools webpaste company-anaconda anaconda-mode web-mode spaceline-all-the-icons all-the-icons-dired all-the-icons neotree meghanada lsp-python lsp-java lsp-mode htmlize org-preview-html evil-search-highlight-persist fortune-cookie org-bullets auctex multi-term restart-emacs markdown-mode dired-subtree evil-mc groovy-mode projectile-ripgrep ripgrep dumb-jump ctags persp-mode spaceline spacemacs-theme silkworm-theme pandoc-mode znc hackernews ibuffer-projectile auto-highlight-symbol hy-mode company-quickhelp nlinum rainbow-mode eyebrowse shackle zone-matrix zone-rainbow zone-nyan company-irony auto-complete-etags ag ggtags circe cider highlight-sexp highlight-parentheses zoom-frm fill-column-indicator powerline-evil ace-window evil-magit hydra helm-anything magit use-package projectile evil elpy color-theme-sanityinc-tomorrow)))
+ '(projectile-mode t nil (projectile))
  '(vc-annotate-background nil)
  '(vc-annotate-color-map
    (quote
